b611fbb2e2ca2c615ca73462c5351eba2ba7b38e
Bruno Gonçalves Ferreira <brunogoncalvesferreira@outlook.com>
2023-02-09 23:07:44 -0300

    :boom: install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontSize: {
+      s: "80.5%",
+    },
+    extend: {
+      fontFamily: {
+        mono: "JetBrains Mono",
+      },
+    },
+  },
+  plugins: [],
+}
