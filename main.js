238673d163e284fcb1f482eb1158fe5d6421461a
Bruno Gonçalves Ferreira <brunogoncalvesferreira@outlook.com>
2023-02-11 15:54:32 -0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-alert("Olá Bem vindo ao pesquisa Gihtub")
+let input = document.querySelector("input")
+let buttonGetUserGithub = document.querySelector("#buttonGetUserName")
+
+buttonGetUserGithub.addEventListener("click", handleGetUserGithub)
+
+function handleGetUserGithub() {
+  let nameUserGithub = input.value
+
+  async function fetchData() {
+    const response = await fetch(
+      `https://api.github.com/users/${nameUserGithub}`
+    )
+    const data = await response.json()
+    document.querySelector("#avatar").src = data.avatar_url
+    document.querySelector("#name").innerHTML = data.name
+    document.querySelector("#login").innerHTML = data.login
+    document.querySelector("#bio").innerHTML = data.bio
+    document.querySelector("#date").innerHTML = document.querySelector(
+      "#repos"
+    ).innerHTML = data.public_repos
+    document.querySelector("#followers").innerHTML = data.followers
+    document.querySelector("#following").innerHTML = data.following
+    document.querySelector("#locale").innerHTML = data.location
+    document.querySelector("#social").innerHTML = data.twitter_username
+    document.querySelector("#page").innerHTML = data.blog
+    document.querySelector("#jobs").innerHTML = data.company
+    console.log(data)
+  }
+  fetchData()
+}
